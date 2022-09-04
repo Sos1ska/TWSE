5311a426c399574c35990bdfa251970bf552f20d
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2022-09-04 10:26:58 +0300

    Update ccf.py

--- files/ccf.py
+++ files/ccf.py
@@ -1,1 +1,30 @@
+import io, zipfile
+from json import dump, dumps, load, loads
+from important_func import load_pyc_files
 
+def CreateSYSConfig():
+    ram=io.BytesIO()
+    structure = load_pyc_files(r'structure_configs.pyc')
+    with zipfile.ZipFile(ram, 'w') as fileinram:
+        with io.BytesIO() as json_ram:
+            data=dumps(structure.systemC())
+            json_ram.write(data.encode())
+            json_ram.seek(0)
+            
+            fileinram.writestr('system.json', json_ram.read())
+    with open(r'files\buffer\system.zip', 'wb') as file:
+        ram.seek(0)
+        file.write(ram.read())
+def CreateCOLConfig():
+    ram=io.BytesIO()
+    structure = load_pyc_files(r'structure_configs.pyc')
+    with zipfile.ZipFile(ram, 'w') as fileinram:
+        with io.BytesIO() as json_ram:
+            data=dumps(structure.colorC())
+            json_ram.write(data.encode())
+            json_ram.seek(0)
+            
+            fileinram.writestr('color.json', json_ram.read())
+    with open(r'files\buffer\color.zip', 'wb') as file:
+        ram.seek(0)
+        file.write(ram.read())
