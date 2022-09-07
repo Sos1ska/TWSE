e6d7a750b5ace2f0596e240b9660622a2c166ac1
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2022-09-07 20:30:36 +0300

    Update important_func.py

--- files/important_func.py
+++ files/important_func.py
@@ -10,12 +10,10 @@ def load_pyc_files(filepath):
     return __import__(module)
 
 def unzip_config():
-    with zipfile.ZipFile(r'files\buffer\color.zip') as archive1:
-        archive1.extractall(r'files\config')
     with zipfile.ZipFile(r'files\buffer\system.zip') as archive2:
         archive2.extractall(r'files\config')
-        
+
 def delete_config():
-    files = ['system.json', 'color.json']
+    files = ['system.json']
     for file in files:
         os.remove(r'files\config\%s' % (file))
