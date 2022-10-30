09a14b287bebaa3c9a535d245135b4ccc2814fee
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2022-10-30 15:08:14 +0300

    Add files via upload

---
+++ files/core/__init__.py
@@ -0,0 +1,10 @@
+from .LOGer import _debug_ as debug
+from .LOGer import _error_ as error
+from .LOGer import _warning_ as warning
+from .LOGer import _info_ as info
+from .LOGer import autolog, autolog_color
+from .LOGer import _debug_color_ as debug_color
+from .LOGer import _error_color as error_color
+from .LOGer import _warning_color as warning_color
+from .LOGer import _info_color as info_color
+from .requests import get
