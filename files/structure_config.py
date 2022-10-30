4443dea6cb67fb5a6cc456d28a6df8bdd77b7408
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2022-10-30 15:06:35 +0300

    Create structure_config.py

---
+++ files/structure_config.py
@@ -0,0 +1,29 @@
+def systemC():
+    a = {
+    "Start Clear Window":"Yes",
+    "Start Show Banner":"Yes",
+    "Clear Every Time":"Yes",
+    "DebugMode":"Yes",
+    "Record Info in DataBase":"No",
+    "Ways":{
+        "info":"files\\log\\info.log",
+        "error":"files\\log\\error.log",
+        "debug":"files\\log\\debug.log",
+        "warning":"files\\log\\warn.log",
+        "general":"files\\log\\genera.log"
+    }
+}
+    return a
+def color():
+    a = {
+    "Color Banner":"white",
+    "Color Text":"yellow",
+    "Color True Answer":"green",
+    "Color False Answer":"red",
+    "Color Back Banner":"black",
+    "Color Back Text":"black",
+    "Color Back True Answer":"black",
+    "Color Back False Answer":"black"
+}
+
+    return a
