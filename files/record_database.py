fa39f96ac1101c5858ccadbcaab5f447e2642ed3
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2023-01-10 21:28:55 +0300

    Add files via upload

---
+++ files/record_database.py
@@ -0,0 +1,16 @@
+import sqlite3
+from .important_func import path_os
+
+def _insert(way, table, *info):
+    data_local=info
+    try : connect_db=sqlite3.connect(path_os(way))
+    except sqlite3.DatabaseError : print(f'[ record_database ] - [ Not found DataBase -> {path_os(way)} ]')
+    cur=connect_db.cursor()
+    match table:
+        case "IP":
+            cur.execute("""INSERT INTO IP VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""", data_local)
+        case "Number":
+            cur.execute("""INSERT INTO Number_phone VALUES(?, ?, ?, ?, ?, ?)""", data_local)
+        case "MAC":
+            cur.execute("""INSERT INTO MAC VALUES(?, ?, ?)""", data_local)
+        
