ef2de5a2aee8ebbe295be798e33ba0031b9dcd12
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2023-01-24 17:47:57 +0300

    Update twse.py

--- twse.py
+++ twse.py
@@ -1,6 +1,5 @@
 __author__ = '\nSos1ska\nhttps://github.com/Sos1ska\nhttps://vk.com/nikitasos1ska'
 __code__ = '\n\nOpenSourceCode'
 
-from typing import Any
-from files.main import Console
-Console.__main__(Any)
+from files.main import _console
+_console()
